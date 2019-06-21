ad1ce39067534b92467686e6c3a166c64298cb01
pairavanvvl <36874538+pairavanvvl@users.noreply.github.com>
2019-06-21 15:47:50 +0530

    Add files via upload

---
+++ src/DocServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace thincscape\docxtotext;
+
+use Illuminate\Support\ServiceProvider;
+
+class DocServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+       
+    }
+
+    public function register()
+    {
+    }
+}
